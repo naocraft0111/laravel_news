bd28c14ce7ad10755c5f09ccda4d6a019313aedb
naocraft0111 <naoki1306@gmail.com>
2022-10-22 21:48:14 +0900

    記事投稿作成

--- src/app/Http/Controllers/User/PostController.php
+++ src/app/Http/Controllers/User/PostController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -13,6 +16,7 @@ class PostController extends Controller
     public function __construct()
     {
         $this->post = new Post();
+        $this->category = new Category();
     }
 
     /**
@@ -26,5 +30,52 @@ class PostController extends Controller
         // ユーザーIDと一致する投稿データを取得
         $posts = $this->post->getAllPostsByUserId($id);
         return view('user.list.index', compact('posts'));
+    }
+
+    /**
+     * 記事投稿画面
+     */
+    public function create()
+    {
+        $categories = $this->category->getAllCategories();
+        return view('user.list.create', compact(
+            'categories',
+        ));
+    }
+
+    /**
+     * 記事投稿処理
+     *
+     * @param $request リクエストデータ
+     * @return Response src/resources/views/user/list/index.blade.phpを表示
+     */
+    public function store(PostRequest $request)
+    {
+        // ログインしているユーザー情報を取得
+        $user = Auth::user();
+        // ログインユーザー情報からユーザーIDを取得
+        $user_id = $user->id;
+
+        switch (true) {
+            // 下書き保存クリック時の処理
+            case $request->has('save_draft'):
+                $this->post->insertPostToSaveDraft($user_id, $request);
+                break;
+            // 公開クリック時の処理
+            case $request->has('release'):
+                $this->post->insertPostToRelease($user_id, $request);
+                break;
+            // 予約公開クリック時の処理
+            case $request->has('reservation_release'):
+                $this->post->insertPostToReservationRelease($user_id, $request);
+                break;
+            // 上記以外の処理
+            default:
+                $this->post->insertPostToSaveDraft($user_id, $request);
+                break;
+        }
+
+
+        return to_route('user.index', ['id' => $user_id]);
     }
 }
